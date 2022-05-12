7594eabe7687e701800c0cefde9db6b59303c52f
kashyapamit856 <kashyap.amit856@gmail.com>
2022-05-12 12:06:26 +0530

    uplaod Full Project

---
+++ eAM-Medical/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eAM_Medical.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
