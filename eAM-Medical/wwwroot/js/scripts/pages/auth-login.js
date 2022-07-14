aa4605f8a72faf9cc2a96e5b0828737364e0ed5d
Amit Kashyap <kashyap.amit856@gmail.com>
2022-07-14 18:23:43 +0530

    Add Validation In Input Box

--- eAM-Medical/wwwroot/js/scripts/pages/auth-login.js
+++ eAM-Medical/wwwroot/js/scripts/pages/auth-login.js
@@ -7,34 +7,55 @@
   Author URL: http://www.themeforest.net/user/pixinvent
 ==========================================================================================*/
 
-$(function () {
-  'use strict';
+//$(function () {
+//  'use strict';
 
-  var pageLoginForm = $('.auth-login-form');
+//  var pageLoginForm = $('.auth-login-form');
 
-  // jQuery Validation
-  // --------------------------------------------------------------------
-  if (pageLoginForm.length) {
-    pageLoginForm.validate({
-      /*
-      * ? To enable validation onkeyup
-      onkeyup: function (element) {
-        $(element).valid();
-      },*/
-      /*
-      * ? To enable validation on focusout
-      onfocusout: function (element) {
-        $(element).valid();
-      }, */
-      rules: {
-        'login-email': {
-          required: true,
-          email: true
-        },
-        'login-password': {
-          required: true
-        }
-      }
-    });
-  }
-});
+//  // jQuery Validation
+//  // --------------------------------------------------------------------
+//  if (pageLoginForm.length) {
+//    pageLoginForm.validate({
+//      /*
+//      * ? To enable validation onkeyup
+//      onkeyup: function (element) {
+//        $(element).valid();
+//      },*/
+//      /*
+//      * ? To enable validation on focusout
+//      onfocusout: function (element) {
+//        $(element).valid();
+//      }, */
+//      rules: {
+//        'login-email': {
+//          required: true,
+//          email: true
+//        },
+//        'login-password': {
+//          required: true
+//        }
+//      }
+//    });
+//  }
+//});
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})();
